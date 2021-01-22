011e8a899eeb8dc7d792b4cb845b1f4f30792f57
juanp-ctrl <juanpa11121@hotmail.com>
2021-01-22 08:54:20 -0600

    Practica en clase de punteros en c

---
+++ Unidad 1 - Lenguaje C/Practica/Ejercicio.c
@@ -0,0 +1,30 @@
+#include <stdio.h>  //Incluimos el prototipo de las funciones de input y output
+#include <stdlib.h>
+
+void functionPointer(int *, int); //Definimos el prototipo de la funcion, necesita 
+
+int main(void){
+
+    int variable = 20;
+    printf("variable = %d\n", variable);
+
+    //Con el asterisco almacenamos direcciones y con & obtenemos la direccion de la variable
+    //El puntero puede cambiar la direccion de la variable
+    int *pvariable = &variable;
+
+    *pvariable = 30;
+
+    printf("Variable = %d\n", variable);
+
+    return EXIT_SUCCESS;
+}
+
+void functionPointer(int * pvar, int value){
+
+    *pvar = (*pvar) + value;
+}
+
+/*
+El puntero es donde guardamos la direccion de la memoria
+podemos usar el puntero para interactuar con la variable
+*/
