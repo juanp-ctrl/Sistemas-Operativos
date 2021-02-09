393318b9c26cf1665a10772614fb3ff4eb02aa44
juanp-ctrl <juanpa11121@hotmail.com>
2021-02-09 16:09:03 -0600

    Ejercicio 9

--- Unidad 1 - Lenguaje C/Ejercicio 9/src/Ejercicio 9.c
+++ Unidad 1 - Lenguaje C/Ejercicio 9/src/Ejercicio 9.c
@@ -11,7 +11,17 @@
 #include <stdio.h>
 #include <stdlib.h>
 
-int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+int main(void)
+{
+    int a;
+    printf("enter a number: ");
+    int n = scanf("%d", &a);
+    printf("%d",n);
+    while (scanf("%d", &a) != 1)  //la conversion no se realiza si es algo que no sea numeros y en este caso devuelve 0
+    {
+        // input was not a number, ask again:
+        printf("enter a number: ");
+    }
+    printf("You entered %d.\n", a);
 	return EXIT_SUCCESS;
 }
