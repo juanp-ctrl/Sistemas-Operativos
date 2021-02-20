97cd03c821e9235005891f2041c3cb63c9331c05
juanp-ctrl <juanpa11121@hotmail.com>
2021-02-20 13:23:05 -0600

    Funcionamiento basico de llamado de comandos en el programa

--- Unidad 1 - Lenguaje C/Proyecto 1/src/Proyecto 1.c
+++ Unidad 1 - Lenguaje C/Proyecto 1/src/Proyecto 1.c
@@ -10,8 +10,31 @@
 
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 
 int main(void) {
-	puts("!!!Hello World!!!"); /* prints !!!Hello World!!! */
+	puts("!!!Base de datos de Estudiantes!!!");
+
+	char entrada[100], comand[40];
+	int salida = 0;
+
+	do{
+		printf("Ingresa el comando \n");
+		fgets(entrada, 99, stdin);
+
+		sscanf(entrada, "%s", comand);
+
+		if(strncmp(comand, "mkdb", 4) == 0){
+			printf("Base de datos creada\n");
+		}
+		else if(strncmp(comand, "exit", 4) == 0){
+			salida = 1;
+		}
+		else{
+			printf("Comando Incorrecto");
+		}
+
+	}while(salida == 0);
+
 	return EXIT_SUCCESS;
 }
