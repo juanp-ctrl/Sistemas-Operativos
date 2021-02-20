69ed858051675696eb819ae03249239bb82252f3
juanp-ctrl <juanpa11121@hotmail.com>
2021-02-20 14:02:48 -0600

    Aplicamos uso de estructuras y heap en el programa

--- Unidad 1 - Lenguaje C/Proyecto 1/src/Proyecto 1.c
+++ Unidad 1 - Lenguaje C/Proyecto 1/src/Proyecto 1.c
@@ -15,8 +15,14 @@
 int main(void) {
 	puts("!!!Base de datos de Estudiantes!!!");
 
-	char entrada[100], comand[40];
-	int salida = 0;
+	typedef struct estudiantes{
+		int cedula;
+		char nombre[20];
+		int semestre;
+	}estudiante;
+
+	char entrada[100], comand[40], nombredb[20];
+	int salida = 0, tamaño;
 
 	do{
 		printf("Ingresa el comando \n");
@@ -25,7 +31,10 @@ int main(void) {
 		sscanf(entrada, "%s", comand);
 
 		if(strncmp(comand, "mkdb", 4) == 0){
-			printf("Base de datos creada\n");
+			sscanf(entrada, "%s %s %d", comand, nombredb, &tamaño);
+			estudiante *puntr;
+			puntr = malloc(sizeof(estudiante)* tamaño);
+			printf("Base de datos creada exitosamente\n");
 		}
 		else if(strncmp(comand, "exit", 4) == 0){
 			salida = 1;
