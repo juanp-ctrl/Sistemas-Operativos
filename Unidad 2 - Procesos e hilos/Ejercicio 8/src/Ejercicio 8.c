909bab882a43ad14e396834066a104d5d40afe39
juanp-ctrl <juanpa11121@hotmail.com>
2021-03-20 16:16:17 -0500

    Ejercicio 8 con manejo de errores

--- Unidad 2 - Procesos e hilos/Ejercicio 8/src/Ejercicio 8.c
+++ Unidad 2 - Procesos e hilos/Ejercicio 8/src/Ejercicio 8.c
@@ -12,12 +12,11 @@
 #include <stdlib.h>
 #include <wait.h>
 #include <unistd.h>
+#include <errno.h>
 
 int main(int argc, char *argv[]) {
 
-	pid_t hijo1;
-	pid_t hijo2;
-	pid_t hijo3;
+	int id = 0;
 
 	char *numeros[argc];
 	for(int i=0; i<argc; i++){
@@ -25,26 +24,46 @@ int main(int argc, char *argv[]) {
 	}
 	numeros[argc] = NULL;
 
-	hijo1 = fork();
-	if(wait(NULL) == hijo1){
-		hijo2 = fork();
-		if(wait(NULL) == hijo2){
-			hijo3 = fork();
-			if(wait(NULL) == hijo3){
-				puts("Proceso finalizado");
-			}
-		}
+	for (int hijo = 0; hijo < 3; ++hijo) {
+
+	    int pid = fork();
+
+	    if(pid < 0){
+	        exit(EXIT_FAILURE);
+	    }
+	    else if (pid > 0){
+	    	id++;
+
+	    }
+	    else{
+	        if(id == 0){
+	        	if(execv("./promedio", numeros) == -1){
+	        		perror("Error de ejecución");
+	        		return(1);
+	        	}
+	    	}
+	    	else if(id == 1){
+	        	if(execv("./min", numeros) == -1){
+	        		perror("Error de ejecución");
+	        		return(1);
+	        	}
+	    	}
+	    	else if(id == 2){
+	        	if(execv("./max", numeros) == -1){
+	        		perror("Error de ejecución");
+	        		return(1);
+	        	}
+	    	}
+
+	        exit(EXIT_SUCCESS);
+	    }
 	}
 
-	if(hijo1 == 0){
-		execv("./promedio", numeros);
+	for (int hijo = 0; hijo < 3; ++hijo) {
+	    int status;
+	    pid_t pid = wait(&status);
 	}
-	else if(hijo2 == 0){
-		execv("./min", numeros);
-	}
-	else if(hijo3 == 0){
-		execv("./max", numeros);
-	}
+	puts("Proceso finalizado");
 
 	return EXIT_SUCCESS;
 }
