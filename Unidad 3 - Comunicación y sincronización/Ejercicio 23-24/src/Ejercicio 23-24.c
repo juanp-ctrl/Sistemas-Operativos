05bef2fd986cb55dc6fbeaf98b77b304d494a7df
juanp-ctrl <juanpa11121@hotmail.com>
2021-04-24 20:34:14 -0500

    Ejercicio 24

--- Unidad 3 - Comunicación y sincronización/Ejercicio 23-24/src/Ejercicio 23-24.c
+++ Unidad 3 - Comunicación y sincronización/Ejercicio 23-24/src/Ejercicio 23-24.c
@@ -1,13 +1,3 @@
-/*
- ============================================================================
- Name        : Ejercicio.c
- Author      : Juan Pablo Jiménez Heredia
- Version     :
- Copyright   : Your copyright notice
- Description : Hello World in C, Ansi-style
- ============================================================================
- */
-
  #include <stdio.h>
  #include <stdint.h>
  #include <stdlib.h>
@@ -17,100 +7,135 @@
  #include <string.h>
  #include <sys/mman.h>
  #include <sys/wait.h>
+ #include <semaphore.h>
 
  #define SH_SIZE 4
 
- int shared_fd = -1;
+ int shared_fd = 0;
 
- int32_t* counter = NULL;
+ int32_t* contador = NULL;
+ sem_t* semaforo = NULL;
 
+ void init_control_mechanism() {
+
+     semaforo = sem_open("semaforo", O_CREAT, 0600, 1);		//Creamos el semaforo
+
+     if (semaforo == SEM_FAILED) {	//Si falla
+         perror("Opening the semaphore failed: ");
+         exit(EXIT_FAILURE);
+     }
+
+ }
+
+ void shutdown_control_mechanism() {
+
+     if (sem_close(semaforo) < 0) {		//Cerramos el semaforo
+         perror("Closing the semaphore failed: ");
+         exit(EXIT_FAILURE);
+     }
+
+     if (sem_unlink("sem0") < 0) {		//Eliminamos el semaforo
+         perror("Unlinking failed: ");
+         exit(EXIT_FAILURE);
+     }
+ }
 
  void init_shared_resource() {
 
-	 shared_fd = shm_open("shm0", O_CREAT | O_RDWR, 0600);
+     shared_fd = shm_open("shm0", O_CREAT | O_RDWR, 0600);		//Creamos el archivo de memoria compartida
 
-	 if (shared_fd < 0) {
-		 perror("Failed to create shared memory: ");
-		 exit(EXIT_FAILURE);
-	 }
+     if (shared_fd < 0) {
+         perror("Failed to create shared memory: ");
+         exit(EXIT_FAILURE);
+     }
 
-	 fprintf(stdout, "Memoria compartida es creada con fd: %d\n", shared_fd);
+     fprintf(stdout, "La memoria compartida es creada con fd: %d\n", shared_fd);
 
  }
 
  void shutdown_shared_resource() {
 
-	 if (shm_unlink("shm0") < 0) {
-		 perror("Unlinking shared memory failed: ");
-		 exit(EXIT_FAILURE);
-	 }
+     if (shm_unlink("shm0") < 0) {
+         perror("Unlinking shared memory failed: ");
+         exit(EXIT_FAILURE);
+     }
 
  }
 
  void inc_counter() {
-	 usleep(1);
-	 int32_t temp = *counter;
-	 usleep(1);
-	 temp++;
-	 usleep(1);
-	 *counter = temp;
-	 usleep(1);
+
+     usleep(1);
+
+     sem_wait(semaforo);		//Bloqueamos con el semaforo
+     int32_t temp = *contador;
+
+     usleep(1);
+     temp++;
+
+     usleep(1);
+     *contador = temp;
+
+     sem_post(semaforo);		//Desbloqueamos con el semaforo
+     usleep(1);
+
  }
 
 
  int main(int argc, char** argv) {
 
-	 init_shared_resource();
+     init_shared_resource();
+     init_control_mechanism();
 
-	 if (ftruncate(shared_fd, SH_SIZE * sizeof(char)) < 0) {
-		 perror("Truncation failed: ");
-		 exit(EXIT_FAILURE);
-	 }
+     if (ftruncate(shared_fd, SH_SIZE * sizeof(char)) < 0) {
+         perror("Truncation failed: ");
+         exit(EXIT_FAILURE);
+     }
 
-	 fprintf(stdout, "La region de memoria ha sido truncada.\n");
+     fprintf(stdout, "La region de memoria ha sido truncada.\n");
 
-	 void* map = mmap(0, SH_SIZE, PROT_WRITE, MAP_SHARED, shared_fd, 0);
+     void* map = mmap(0, SH_SIZE, PROT_WRITE, MAP_SHARED, shared_fd, 0);
 
-	 if (map == MAP_FAILED) {
-		 perror("Mapping failed: ");
-		 exit(EXIT_FAILURE);
+     if (map == MAP_FAILED) {
+         perror("Mapping failed: ");
+         exit(EXIT_FAILURE);
+     }
 
- }
+     contador = (int32_t*)map;
+     *contador = 0;
 
- counter = (int32_t*)map;
- *counter = 0;
+     pid_t pid = fork();
 
- pid_t pid = fork();
+     if (pid) { // The parent process
 
- if (pid) { // The parent process
+         inc_counter();
+         fprintf(stdout, "Proceso padre, contador = %d.\n", *contador);
+         int status = -1;
+         wait(&status);
 
-	 inc_counter();
-     fprintf(stdout, "El proceso padre ve el contador = %d.\n", *counter);
-     int status = -1;
-     wait(&status);
-     fprintf(stdout, "El proceso hijo termino con status = %d.\n", status);
+         fprintf(stdout, "El proceso hijo termina con status %d.\n", status);
 
- }
+     } else { // The child process
 
- else { // The child process
-     inc_counter();
-     fprintf(stdout, "El proceso hijo ve el contador = %d.\n", *counter);
- }
+         inc_counter();
+         fprintf(stdout, "Proceso hijo, contador = %d.\n", *contador);
+
+     }
 
 
- if (munmap(counter, SH_SIZE) < 0) {
-     perror("Unmapping failed: ");
-     exit(EXIT_FAILURE);
- }
+     if (munmap(contador, SH_SIZE) < 0) {
+         perror("Unmapping failed: ");
+         exit(EXIT_FAILURE);
+     }
 
- if (close(shared_fd) < 0) {
-     perror("Closing shared memory fd failed: ");
-     exit(EXIT_FAILURE);
- }
+     if (close(shared_fd) < 0) {
+         perror("Closing shared memory fd failed: ");
+         exit(EXIT_FAILURE);
+     }
 
- if (pid) {
-     shutdown_shared_resource();
- }
+     if (pid) {
+         shutdown_shared_resource();
+         shutdown_control_mechanism();
+     }
 
- exit(EXIT_SUCCESS);
+     exit(EXIT_SUCCESS);
  }
