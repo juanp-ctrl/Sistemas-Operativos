fafa47b696e48996dd688ccaa6fdfc158c799ad2
juanp-ctrl <juanpa11121@hotmail.com>
2021-05-13 22:16:17 -0500

    Version funcional del proyecto 2

---
+++ Unidad 4 - Entrada y Salida/Proyecto 2/src/Proyecto 2.c
@@ -0,0 +1,312 @@
+/*
+ ============================================================================
+ Name        : Proyecto.c
+ Author      : Juan Pablo Jiménez H
+ Version     :
+ Copyright   : Your copyright notice
+ Description : Hello World in C, Ansi-style
+ ============================================================================
+ */
+
+#include <stdio.h>
+#include <string.h>
+#include <errno.h>
+#include <unistd.h>
+#include <stdlib.h>
+#include <pthread.h>
+#include <sys/socket.h>
+#include <netinet/in.h>
+#include <signal.h>
+
+#define PORT 8888
+#define BUF_SIZE 128
+
+int maxC = 10, cont = 0;
+struct client_t* client;
+
+struct client_t{
+    int socket;
+    char nombre [64];
+    int estado;
+    int conectado;
+    int numClientes;
+};
+
+void *comandos(void *arg){
+   char comando [32], cliente [32], buf[BUF_SIZE];
+   int status, control, encontro = 0;
+
+   while(strcmp(comando,"exit") != 0){
+
+       printf("\nComandos:\n/cl para ver los clientes\n/del para desconectar un cliente\n");
+
+       scanf("%s",comando);
+       if (strcmp(comando,"/del") == 0){
+
+           scanf("%s",cliente);
+           for(int i=0; i<=maxC; i++){
+
+               if(strcmp((client+i)->nombre,cliente) == 0 && (client+i)->conectado == 1){
+
+                   (client+i)->conectado = 0;
+                   (client+i)->estado = 0;
+                   cont--;
+                   control=i;
+                   encontro=1;
+                   break;
+               }
+           }
+
+           if(encontro==1){
+
+               for(int i=0; i<=maxC; i++){
+
+            	   if((client+i)->conectado == 1 && (client+i)->numClientes != control ){
+
+                       printf(" envio a %s",(client+i)->nombre);
+                       strcpy(buf, (client+control)->nombre);
+                       strcat(buf, " ha abandonado el chat\n");
+                       status = write((client+i)->socket,buf, strlen(buf)+1);
+                       if(status == -1){
+                           perror("Error al escribir en los clientes");
+                           break;
+                       }
+                   }
+                   if((client+i)->numClientes == control){
+
+                	   strcpy(buf, "Te has desconectado\n");
+                       status = write((client+control)->socket, buf, strlen(buf)+1);
+                       if(status == -1){
+						  perror("Error al escribir en el cliente ha desconectar");
+						  break;
+                       }
+                       close((client+control)->socket);
+                   }
+               }
+           }
+
+           else{
+               printf("Usuario no encontrado\n");
+           }
+
+           encontro=0;
+
+       }
+
+       else if (strcmp(comando,"/cl") == 0){
+
+           if(cont == 0){
+               printf("No hay clientes conectados\n");
+           }
+
+           else{
+               printf("Clientes en linea\n");
+
+               for(int i=0; i<maxC; i++){
+                   if((client+i)->conectado != 0){
+                       printf("%s\n",(client+i)->nombre);
+                   }
+               }
+           }
+           printf("En linea: %d\n", cont);
+
+       }
+
+       else if(strcmp(comando,"exit") == 0){
+           printf("\n");
+           break;
+       }
+
+       else{
+           printf("Comando no valido\n");
+       }
+
+    }
+
+   return 0;
+}
+
+void * leer(void *arg){
+
+   struct client_t *clienteC = ((struct client_t *)arg);
+   ssize_t nBytes;
+   char buf[BUF_SIZE], bvd[BUF_SIZE];
+   int status;
+
+   nBytes = read(clienteC->socket, buf, BUF_SIZE);
+
+   strcpy((client+(cont-1))->nombre, buf);		//Asignamos el nombre
+
+   strcpy(bvd,buf);
+   strcat(bvd, " se ha unido al chat");
+
+   for(int i=0; i<=maxC; i++){		//Le informamos a los clientes de la nueva conexion
+
+       if ((client+i)->conectado == 1){
+
+           status = write((client+i)->socket, bvd, strlen(bvd)+1);
+
+           if(status == -1){
+               perror("Error al escribir en los clientes");
+               break;
+           }
+       }
+   }
+
+
+   while(1){		//Leemos los mensajes del cliente
+
+       nBytes = read(clienteC->socket, buf, BUF_SIZE);
+
+       if(nBytes == 0){
+           printf("El cliente cerro la conexión\n");
+           break;
+       }
+       else if(nBytes == -1){
+    	   perror("Error al leer del cliente");
+           return 0;
+       }
+       else {
+           if (clienteC->conectado == 1){
+
+           strcat(buf, " -(");
+           strcat(buf, clienteC->nombre);
+           strcat(buf, ")");
+
+               for(int i=0; i<=maxC; i++){		//Escribimos el mensaje en todos los clientes
+                   if ((client+i)->conectado == 1){
+                       status = write((client+i)->socket, buf, strlen(buf)+1);
+                   }
+               }
+
+               if(status == -1){
+                   perror("Error al escribir en los clientes");
+                   break;
+               }
+           }
+           else{
+               printf("Cliente desconectado\n");
+
+           }
+       }
+   }
+
+   clienteC->estado = 0;
+   close(clienteC->socket);
+
+   return NULL;
+   }
+
+int main(int argc, char *argv[]){
+
+    char buf[BUF_SIZE];
+    int status;
+    int enable = 1;
+    int server_sd;
+    int client_sd;
+    pthread_t hiloComandos;
+    pthread_t rxThreadId [maxC];
+
+
+    client = malloc(sizeof(struct client_t)*maxC);
+
+    // 1. Ignore SIGPIPE
+    signal(SIGPIPE, SIG_IGN);
+
+    // 2. Create socket
+    server_sd = socket(AF_INET, SOCK_STREAM, 0);
+    if (server_sd == -1) {
+        perror("Error al crear el socket\n");
+        exit(EXIT_FAILURE);
+    }
+
+    // 3. Desactivamos el checkeo de direcciones del bind
+    status = setsockopt(server_sd, SOL_SOCKET, SO_REUSEADDR,(int *) &enable, sizeof(enable));
+    if (status == -1){
+        perror("Error al usar setsockopt");
+        exit(EXIT_FAILURE);
+    }
+
+    //4. BIND the socket to an address
+    struct sockaddr_in addr;
+    memset(&addr, 0, sizeof(addr));
+    addr.sin_family = AF_INET;
+    addr.sin_addr.s_addr = INADDR_ANY;
+    addr.sin_port = htons(PORT);
+
+    status = bind(server_sd, (struct sockaddr*)&addr, sizeof(addr));
+
+    if (status == -1) {
+        perror("Error al hacer el bind");
+        close(server_sd);
+        exit(EXIT_FAILURE);
+    }
+    printf("Socket binded\n");
+
+     // 5. Set backlog
+    status = listen(server_sd, 10);
+
+    if (status == -1) {
+        perror("Error al poner en escucha al servidor");
+        close(server_sd);
+        exit(EXIT_FAILURE);
+    }
+
+    for(int i=0; i<=maxC; i++){		//Seteamos la conexion de todos los clientes en cero
+        (client+i)->conectado = 0;
+    }
+
+    printf("Servidor escuchando\n");
+
+    status = pthread_create(&hiloComandos, NULL, &comandos, NULL);	//Creamos el hilos para recibir comandos
+
+    if(status == -1){
+        perror("Error al crear el hilo de comandos");
+        close(server_sd);
+        exit(EXIT_FAILURE);
+    }
+
+    while(1){
+
+        printf("Esperando por clientes\n");
+
+        client_sd = accept(server_sd, NULL, NULL);
+
+        if(client_sd == -1){
+            perror("Error al aceptar la conexion del cliente");
+            close(server_sd);
+            exit(EXIT_FAILURE);
+        }
+
+         // 7. Creamos el hilo para las conexiones del cliente
+        (client+cont)->socket = client_sd;
+        (client+cont)->estado = 1;
+        (client+cont)->conectado = 1;
+        (client+cont)->numClientes = cont;
+
+        if(cont < 10){
+            status = pthread_create(&rxThreadId[cont], NULL, &leer, (client+cont));
+            cont++;
+        }
+        else{
+            printf("Limite de conexiones es 10");
+            close(client_sd);
+        }
+
+        if(status == -1){
+            perror("Error al crear el hilo de lectura");
+            close(server_sd);
+            exit(EXIT_FAILURE);
+        }
+
+        if(client->estado == 0){
+			printf("El cliente cerro la conexión\n");
+			break;
+		}
+
+    }
+
+    free(client);
+
+    exit(EXIT_SUCCESS);
+}
---
+++ Unidad 4 - Entrada y Salida/Proyecto 2/src/cliente.c
@@ -0,0 +1,152 @@
+/*
+ * cliente.c
+ *
+ *  Created on: May 13, 2021
+ *      Author: juanpa
+ */
+
+
+ #include <stdio.h>
+ #include <string.h>
+ #include <errno.h>
+ #include <unistd.h>
+ #include <stdlib.h>
+ #include <pthread.h>
+ #include <sys/socket.h>
+ #include <netinet/in.h>
+ #include <signal.h>
+ #include <arpa/inet.h>
+
+ #define PORT 8888
+ #define BUF_SIZE 128
+
+ struct client_t{
+     int socket;
+     int estado;
+ };
+
+ void * leer(void *arg){
+
+     struct client_t *client = ((struct client_t *)arg);
+
+     ssize_t nBytes;
+     char buf[BUF_SIZE];
+
+     while(1){			//Leemos del servidor
+
+    	 nBytes = read(client->socket, buf, BUF_SIZE);
+
+         if(nBytes == 0){
+             printf("Se ha cerrado el servidor\n");
+             break;
+         }
+
+         else if(nBytes == -1){
+             perror("Error al leer con el hilo");
+             break;
+         }
+
+         else{
+             printf("Server: %s\n",buf);
+         }
+
+     }
+
+     printf("Terminate Pthread for reading\n");
+
+     client->estado = 0;
+     return NULL;
+ }
+
+ int main(int argc, char *argv[]){
+
+     char buf[BUF_SIZE];
+     char nombre[64];
+     int status;
+     int server_sd;
+     pthread_t hilo;
+     struct client_t client;
+
+     printf("Ingresa tu nombre:\n");
+     scanf("%s",nombre);
+
+     // 1. ignoramos la señal sigpipe
+     signal(SIGPIPE, SIG_IGN);
+
+     // 2. Creamos el socket para el server
+     server_sd = socket(AF_INET, SOCK_STREAM, 0);
+
+     if (server_sd == -1) {
+         perror("Error al crear el socket\n");
+         exit(EXIT_FAILURE);
+     }
+
+     printf("Socket creado\n");
+
+     //3. Connect to the server 127.0.0.1:PORT
+
+     struct sockaddr_in addr;
+     memset(&addr, 0, sizeof(addr));
+
+     addr.sin_family = AF_INET;
+     addr.sin_addr.s_addr = inet_addr("127.0.0.1");
+     addr.sin_port = htons(PORT);
+
+     status = connect(server_sd, (struct sockaddr*)&addr, sizeof(addr));		//Nos conectamos al servidor con la direccion dada
+
+     if(status == -1){
+         perror("Error al conectar con el servidor\n");
+         close(server_sd);
+         exit(EXIT_FAILURE);
+     }
+
+     printf("Conectado al servidor\n");
+
+     // 4. Creación del hilo para leer
+     client.socket = server_sd;
+     client.estado = 1;
+     printf("Hilo creado para leer del servidor\n");
+
+     status = pthread_create(&hilo, NULL, &leer, &client);
+
+     if(status == -1){
+         perror("Error al crear el hilo");
+         close(server_sd);
+         exit(EXIT_FAILURE);
+     }
+
+     status = write(client.socket, nombre, strlen(nombre)+1);
+
+	  if(status == -1){
+		  perror("Error al enviar el nombre");
+	  }
+
+     while(1){
+
+         if(client.estado == 0){
+             printf("El servidor cerro el socket\n");
+             break;
+         }
+
+         if (fgets(buf,BUF_SIZE,stdin) == NULL){
+             printf("Entrada nula\n");
+         }
+         if( 0 == strncmp(buf,":exit",strlen(":exit")) ){
+             printf("Client exit\n");
+             break;
+         }
+
+         if( buf[ strlen(buf)-1 ] == '\n') buf[ strlen(buf) - 1 ] = 0;
+
+         status = write(client.socket, buf, strlen(buf)+1);
+
+         if(status == -1){
+             perror("Error al escribir en el servidor");
+             break;
+         }
+     }
+
+     close(client.socket);
+
+     exit(EXIT_SUCCESS);
+ }
